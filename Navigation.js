243c8635ab0e0b59af60436f2aec0eaba3095dcf
maggi-master <82754177+maggi-master@users.noreply.github.com>
2021-09-19 20:18:29 +0200

    Delete Navigation.js

--- Navigation.js
+++
@@ -1,6 +0,0 @@
-document.write("<ul id=\"navigation\">");
-document.write("            <li id=\"list\"><a id=\"button\" class=\"active\" href=\"index.html\">Home<\/a><\/li>");
-document.write("            <li id=\"list\"><a id=\"button\" href=\"about_me.html\">About<\/a><\/li>");
-document.write("            <li id=\"list\"><a id=\"button\" href=\"rubikscube.html\">Rubikscube<\/a><\/li>");
-document.write("            <li id=\"list\"><a id=\"button\" href=\"shutdown_computer.html\">How to shutdown a Computer<\/a><\/li>");
-document.write("        <\/ul>");
